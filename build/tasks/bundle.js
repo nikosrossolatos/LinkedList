14c8c0c4de9ab7808c5e5f55b2db2a14e1bb9206
Nick Rossolatos <spidey_nr@hotmail.com>
2015-10-27 01:32:09 +0200

    cleanup in build tasks, minified lib

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -3,13 +3,17 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var babel = require('babelify');
 var notify = require("gulp-notify");
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
 var paths = require('../paths');
 
-gulp.task('build', function () {
+gulp.task('bundle', function () {
 	return browserify({entries: 'src/index.js', debug: true})
 	.transform(babel)
 	.bundle()
 	.on('error', notify.onError("Error: <%= error.message %>"))
-	.pipe(source('linkedList.js'))
+	.pipe(source('linkedList.min.js'))
+	.pipe(buffer())
+	.pipe(uglify())
 	.pipe(gulp.dest(paths.output));
 });
